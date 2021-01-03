1cad0fc2fdd0f30ba75b633c1c07f4def3e968c5
Novotny Levente <Novotny.Levente@gmail.com>
2021-01-03 14:08:25 +0100

    update start

--- linux/start.sh
+++ linux/start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#Go to root directory
+cd ../
+
 #Delete logs older than 7 days
-find ../logs/ -type f -mtime +7 -name '*.log' -execdir rm -- '{}' \;
+find /logs/ -type f -mtime +7 -name '*.log' -execdir rm -- '{}' \;
 
 #Run node js with logging
-node ../main.js > "$(date +%Y%m%d_%H%M%S).log" 2>&1
+node /main.js > "$(date +%Y%m%d_%H%M%S).log" 2>&1
