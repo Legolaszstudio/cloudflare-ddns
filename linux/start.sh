49b758c33fb03b68e3c50454f5a110e01b6c0d34
Novotny Levente <Novotny.Levente@gmail.com>
2021-01-03 14:04:07 +0100

    Start.sh

---
+++ linux/start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Delete logs older than 7 days
+find ../logs/ -type f -mtime +7 -name '*.log' -execdir rm -- '{}' \;
+
+#Run node js with logging
+node ../main.js > "$(date +%Y%m%d_%H%M%S).log" 2>&1
