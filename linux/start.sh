d11b076fb169e962b14f86da1472ea0012142672
Legolaszstudio <Novotny.Levente@gmail.com>
2021-04-27 08:49:22 +0200

    Update start.sh

--- linux/start.sh
+++ linux/start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+#Get bash file location and cd to it
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
+
 #Go to root directory
 cd ../
 
 #Delete logs older than 7 days
 find logs/ -type f -mtime +7 -name '*.log' -execdir rm -- '{}' \;
+
+#Fix ipv6 errors
+ping6 -c 6 google.hu
 
 #Run node js with logging
 node main.js > logs/"$(date +%Y%m%d_%H%M%S).log" 2>&1
